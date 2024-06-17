a60be1f3252b858bcb825c2db831d30224c9a412
Ceffa93 <ceffa93@gmail.com>
2024-06-17 23:05:09 +0900

    Cleanup

--- Main.cpp
+++ Main.cpp
@@ -1,37 +1,27 @@
 #include "Named.h"
 #include <iostream>
 
-using Width = Named<int, struct _Width>;
-using Height = Named<int, struct _Height>;
-using Area = Named<int, struct _Area>;
+NAMED(Width, int);
+NAMED(Height, int);
+NAMED(Area, int32_t);
 
 struct Rectangle
 {
-    Rectangle(Width, Height);
-    Area getArea();
+    Rectangle(Width width, Height height) : width(width), height(height) {}
+    Area getArea() { return Area(width * height); }
 
     Width width;
     Height height;
 };
 
-Rectangle::Rectangle(Width width, Height height)
-    : width(width), height(height)
-{
-}
-
-Area Rectangle::getArea()
-{
-    return Area(width * height);
-}
-
-using ScreenSize = Named<Rectangle, struct _ScreenSize>;
-using Supersampling = Named<Rectangle, struct _Supersampling>;
-using Resolution = Named<Rectangle, struct _Resolution>;
+NAMED(ScreenSize, Rectangle, struct _ScreenSize>);
+NAMED(Supersampling, Rectangle, struct _Supersampling>);
+NAMED(Resolution, Rectangle, struct _Resolution>);
 
 Resolution getResolution(ScreenSize screenSize, Supersampling supersampling)
 {
-    int w = screenSize.get().width / supersampling.get().width;
-    int h = screenSize.get().height / supersampling.get().height;
+    int w = screenSize->width / supersampling.get().width;
+    int h = (*screenSize).height / ((Rectangle)supersampling).height;
     return Resolution(Width(w), Height(h));
 }
 
@@ -40,9 +30,9 @@ int main()
     ScreenSize screenSize{ Width(1920), Height(1080) };
     Supersampling supersampling{ Width(2), Height(2) };
 
-    Rectangle resolution = getResolution(screenSize, supersampling);
+    Resolution resolution = getResolution(screenSize, supersampling);
 
-    std::cout << "Width: " << resolution.width << std::endl;
-    std::cout << "Height: " << resolution.height << std::endl;
-    std::cout << "PixelCount: " << resolution.getArea() << std::endl;
+    std::cout << "Width: " << resolution->width << std::endl;
+    std::cout << "Height: " << resolution->height << std::endl;
+    std::cout << "PixelCount: " << resolution->getArea() << std::endl;
 }
--- Named.h
+++ Named.h
@@ -1,17 +1,25 @@
 #include <utility>
 
+#define NAMED(Name, Type) using Name = Named<Type, struct _Name>
+
 template <class Type, class Name>
 class Named
 {
 public:
-    template<class... Args>
-    explicit Named(Args&&... args) 
-        : type(std::forward<Args>(args)...) 
-    {}
+     template<class... Args>
+     constexpr explicit Named(Args&&... args)
+         : type(std::forward<Args>(args)...)
+     {}
 
-    operator Type& () { return type; }
+    constexpr operator Type& () { return type; }
 
-    Type& get() { return type; }
+    constexpr Type* operator-> () { return &type; }
+    constexpr const Type* operator-> () const { return &type; }
+
+    constexpr Type& operator* () { return type; }
+    constexpr const Type& operator* () const { return type; }
+
+    constexpr Type& get() { return type; }
 
 private:
     Type type;
